ef51371b09539218eeed91965e3beb74ffea8831
SergeyArenzon <sergeyr1991@gmail.com>
2020-04-16 14:27:16 +0300

    bug fix1

--- FamilyTree.cpp
+++ FamilyTree.cpp
@@ -226,17 +226,17 @@ void family::Tree::remove(string name){
 };
 
 
- int main(){
-    family::Tree T ("Yosef"); // Yosef is the "root" of the tree (the youngest person).
-	T.addFather("Yosef", "Yaakov")  // Tells the tree that the father of Yosef is Yaakov.
-	    .addMother("Yosef", "Rachel") // Tells the tree that the mother of Yosef is Rachel.
-        .addFather("Yaakov", "Isaac")
-	    .addMother("Yaakov", "Rivka")
-	    .addFather("Isaac", "Avraham")
-	    .addFather("Avraham", "Terah");
-        T.addFather("Sergey", "avi");
+//  int main(){
+//     family::Tree T ("Yosef"); // Yosef is the "root" of the tree (the youngest person).
+// 	T.addFather("Yosef", "Yaakov")  // Tells the tree that the father of Yosef is Yaakov.
+// 	    .addMother("Yosef", "Rachel") // Tells the tree that the mother of Yosef is Rachel.
+//         .addFather("Yaakov", "Isaac")
+// 	    .addMother("Yaakov", "Rivka")
+// 	    .addFather("Isaac", "Avraham")
+// 	    .addFather("Avraham", "Terah");
+//         T.addFather("Sergey", "avi");
         
 
 
-     return 0;
-}
+//      return 0;
+// }
